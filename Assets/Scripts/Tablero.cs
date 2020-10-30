ca32633721c51d15e914ed818ace4723c2e12e8e
Ricardo Lázaro <caffeine305@gmail.com>
2020-10-30 02:47:27 -0600

    Tablero puesto

---
+++ Assets/Scripts/Tablero.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablero : MonoBehaviour
+{
+    public int ancho;
+    public int alto;
+    public GameObject escaque;
+    public GameObject centro;
+    private int[,] fondo;
+
+    void Start()
+    {
+        int[,] fondo = new int[ancho,alto];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < ancho; i++)
+        {
+            for (int j = 0; j < alto; j++)
+            {
+                Vector2 pos = new Vector2(i,j);
+                Instantiate(escaque, pos,Quaternion.identity);
+            }
+        }
+
+        Vector2 center = new Vector2(7.4f,4.52f);
+        Instantiate(centro,center,Quaternion.identity);
+    }
+
+}
